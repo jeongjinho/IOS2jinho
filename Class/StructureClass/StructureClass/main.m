08e02eae235341e40d135c659823c1865d1c4b1d
진호정 <yyyqor@gmail.com>
2016-09-19 15:09:26 +0900

    structureClass directory file create

---
+++ Class/StructureClass/StructureClass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StructureClass
+//
+//  Created by 진호정 on 2016. 9. 19..
+//  Copyright © 2016년 iosSchool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
